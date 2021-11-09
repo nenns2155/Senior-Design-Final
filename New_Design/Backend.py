2ea6f848b31feb9718b7908d66707d65ddb00282
Nathan E <nenns@ksu.edu>
2021-11-09 14:07:52 -0600

    Implementation

---
+++ New_Design/Backend.py
@@ -0,0 +1,83 @@
+def Read_Voltage(samples):
+    try:
+        GPIO.setmode(GPIO.BOARD) # set GPIO pin mode to BCM numbering
+        hx = HX711(dout_pin=36, pd_sck_pin=35)
+
+        lastreading = hx._read()
+        count = 0
+        sum = 0
+        while True:
+            reading = hx._read()
+            if reading == -1:
+                lastreading = reading
+                pass
+            elif count == samples:
+                return Voltage_to_Weight(sum/samples)
+            elif reading < 0:
+                pass
+            elif reading == False:
+                pass
+            elif abs(lastreading/reading) > 1.25 or abs(lastreading/reading) < .75:
+                pass
+            else:
+                sum = sum + reading
+                count = count + 1
+            
+            lastreading = reading    
+            
+    
+
+    except (KeyboardInterrupt, SystemExit):
+        print('Bye :)')
+
+def Voltage_to_Weight(voltage):
+
+    A = 190033
+    B = 262378.645
+    C = 256951.0917
+
+    return str((voltage - B)/A)
+
+
+def Calibration():
+
+    print("I should be doing a calibration")
+    # weight = []
+    # voltage = []
+
+    # for i in range(0, 42, 2):
+    #     i=i/4
+    #     input("Press enter when {} lbs are on the scale.".format(i))
+    #     weight.append(i)
+    #     voltagepoint = Read_Voltage(20)
+    #     voltage.append(voltagepoint)
+    #     print("Weight equals:", i)
+    #     print("Voltage equals:",voltagepoint)
+
+
+    # [A,B,C] = np.polyfit(weight, voltage, 2)
+
+    # print('A equals:', A)
+    # print('B equals:', B)
+    # print('C equals:', C)
+
+
+
+def setFeeding(hour, minute, calories):
+
+    print(hour)
+    print(minute)
+    print(calories)
+
+    # import sys
+    # sys.path.append('/home/pi/.local/lib/python2.7/site-packages')
+    
+    # # from crontab import CronTab
+    
+    # cron = CronTab(user = True)
+    # job = cron.new(command = "print('Hit')", comment = str(calories))
+    
+    # job.minute.on(minute)
+    # job.hour.on(hour)
+    # job.dow.every(d)
+    # cron.write()
---
+++ New_Design/Frontend.py
@@ -0,0 +1,148 @@
+import sys
+import os
+import subprocess
+
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QDialog, QMainWindow
+from PyQt5 import QtWidgets
+
+from Main2 import Ui_MainWindow
+from Feeding2 import Ui_Dialog as Ui_Feeding
+from Presets2 import Ui_Dialog as Ui_Presets
+from Development2 import Ui_Dialog as Ui_Development
+
+from Backend import setFeeding, Calibration
+
+import numpy as np
+
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+        ### Further Implementation for MainWindow
+        self.Feeding.clicked.connect(self.open_Feeding)
+        self.Development.clicked.connect(self.open_Development)
+
+
+        ### Ending Further Implementation for MainWindow      
+
+############################################################
+#Feeding Dialog
+############################################################
+    def open_Feeding(self):
+        dialog = QtWidgets.QDialog()
+        Ui_Feeding.setupUi(self, dialog)
+        Ui_Feeding.retranslateUi(self, dialog)
+
+        ### Further Implementation for Ui_Feeding
+        self.Presets.clicked.connect(lambda: self.open_Presets())
+        self.Add.clicked.connect(lambda: GrabValues())
+        self.Add.clicked.connect(lambda: Update_List())
+        
+
+        def GrabValues():
+            calories = self.spinBox.value()
+            hour = self.timeEdit.time().hour()
+            minute = self.timeEdit.time().minute()
+
+            setFeeding(hour, minute ,calories)
+
+            pass
+
+        def Update_List():
+            
+            job = [0,1,2,3,4,5,6,7,8,9,10]
+
+            ##Need to add contab job handling
+
+            for item in job:
+                self.listWidget.insertItem(0,str(item))
+            
+            pass
+
+
+
+        ### Ending Further Implementation for Ui_Feeding
+        
+        dialog.exec_()
+        dialog.show()
+############################################################
+############################################################
+    
+############################################################
+#Preset Dialog
+############################################################
+    def open_Presets(self):
+        dialog = QtWidgets.QDialog()
+        Ui_Presets.setupUi(self, dialog)
+        Ui_Feeding.retranslateUi(self, dialog)
+
+        ### Further Implementation for Ui_Presets
+
+
+        
+        
+        ### Ending Further Implementation for Ui_Presets
+
+        dialog.exec_()
+        dialog.show()
+############################################################
+############################################################
+
+
+############################################################
+#Development Dialog
+############################################################
+    def open_Development(self):
+        dialog = QtWidgets.QDialog()
+        Ui_Development.setupUi(self, dialog)
+        Ui_Development.retranslateUi(self, dialog)
+
+        ### Further Implementation for Ui_Development
+        os.system("hostname -I")
+        ip = subprocess.run(['hostname','-I'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+
+        self.IP.clicked.connect(lambda: self.IP.setText(str(ip)))
+        self.Calibration.clicked.connect(lambda: Calibration())
+
+
+        
+        
+        ### Ending Further Implementation for Ui_Development
+
+        dialog.exec_()
+        dialog.show()
+############################################################
+############################################################
+
+    
+        
+
+# class Feeding(QDialog, Ui_Feeding):
+#     def __init__(self):
+#         super().__init__()
+#         self.setupUi(self)
+
+#         ### slots and signals for mainwindow
+#         self.Add.clicked.connect(self.Presets.setText("wow"))
+
+#     def open_Presets(self):
+#         print("hit")
+#         dialog = QtWidgets.QDialog()
+#         Ui_Presets.setupUi(self, dialog)
+#         Ui_Feeding.retranslateUi(self, dialog)
+#         dialog.exec_()
+#         dialog.show()
+
+
+
+
+
+if __name__ == '__main__':
+    
+    app = QApplication(sys.argv)
+    myapp = MainWindow()
+    myapp.show()
+    sys.exit(app.exec())
