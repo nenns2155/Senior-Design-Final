46ec645112fba15c8d8d3d1e36fd5e3d1e7ff7d9
Nathan E <nenns@ksu.edu>
2021-11-11 17:09:33 -0600

    dev

--- New_Design/Weigh.py
+++ New_Design/Weigh.py
@@ -2,6 +2,12 @@ from Backend import Read_Voltage, Voltage_to_Weight
 import time
 import csv
 
+import RPi.GPIO as GPIO  # import GPIO
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(32, GPIO.OUT)
+
+
 t1 = time.time()
 w = Read_Voltage(20)
 t2 = time.time()
@@ -10,3 +16,5 @@ t = (t1 + t2)/2
 with open('weights.csv', 'a') as file:
     writer = csv.writer(file)
     writer.writerow([t,w])
+
+GPIO.cleanup()
