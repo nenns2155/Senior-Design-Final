61fbac9f8453e2a50ce1c942803c5f2f2191665e
Nathan E <nenns@ksu.edu>
2021-11-11 13:06:03 -0600

    dev

--- New_Design/Frontend.py
+++ New_Design/Frontend.py
@@ -97,7 +97,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
         def run():
             GPIO.output(38, 1)
-            time.sleep(2)
+            time.sleep(3)
             GPIO.output(38, 0)
             pass
 
