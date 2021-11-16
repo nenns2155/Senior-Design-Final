8d1fa9b9eceda20c6000df610479d1f1191269f8
Nathan E <nenns@ksu.edu>
2021-11-16 10:16:46 -0600

    dev

--- New_Design/Weight2.py
+++ New_Design/Weight2.py
@@ -13,7 +13,7 @@ class Ui_Dialog(object):
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
 
-        Dialog.resize(480,360)
+        Dialog.resize(480,300)
 
         Dialog.setWindowFlags(QtCore.Qt.FramelessWindowHint)
 
