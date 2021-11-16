ca80f4742f023efffd50cb59fdfc92424196e633
Nathan E <nenns@ksu.edu>
2021-11-16 10:00:45 -0600

    dev

--- New_Design/Feeding2.py
+++ New_Design/Feeding2.py
@@ -14,7 +14,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
-        Dialog.resize(480, 320)
+        Dialog.resize(480, 300)
         self.buttonBox = QtWidgets.QDialogButtonBox(Dialog)
         self.buttonBox.setGeometry(QtCore.QRect(250, 280, 211, 31))
         self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
